9489214ffec7b92347068fc8e63fb8d33bb4c7f7
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-03-31 10:53:38 +0300

    main:feat: inject config module and finish protecting routes

--- src/api/auth/auth.module.ts
+++ src/api/auth/auth.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { AccessTokenGuard } from './guards/access-token/access-token.guard';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthenticationGuard } from './guards/authentication/authentication.guard';
+import { AuthorizationGuard } from './guards/authorization/authorization.guard';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConfig } from 'src/common/configuration/config';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    { provide: APP_GUARD, useClass: AuthenticationGuard },
+    { provide: APP_GUARD, useClass: AuthorizationGuard },
+    AccessTokenGuard,
+  ],
+  imports: [
+    ConfigModule.forFeature(jwtConfig),
+    JwtModule.registerAsync(jwtConfig.asProvider()),
+  ],
 })
 export class AuthModule {}
