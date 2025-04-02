18e6964b6dfbc9813c1a1075bc7488cdf3ee9ceb
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-04-02 11:48:34 +0300

    main:feat: implement find one user by id provider

--- src/api/users/providers/crud/find-user-by-id.provider.ts
+++ src/api/users/providers/crud/find-user-by-id.provider.ts
@@ -1,8 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
+import { User } from '@prisma/client';
+import { PrismaService } from 'src/common/prisma/prisma.service';
+import { CreateApiResponse } from 'src/lib/utils/create-api-response.util';
 
 @Injectable()
 export class FindUserByIdProvider {
-  findUserById(id: string) {
-    return `This action returns a #${id} user`;
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findUserById(id: string) {
+    let user: User | undefined;
+
+    try {
+      user = await this.prisma.user.findUnique({ where: { id } });
+    } catch (err) {
+      console.log('find user by id', err);
+      throw new InternalServerErrorException(
+        'Unable to fetch a user at the moment. please try again later',
+      );
+    }
+
+    if (!user) {
+      throw new NotFoundException('User not found.');
+    }
+
+    return CreateApiResponse({
+      status: 'success',
+      message: 'fetch user by id successfull',
+      data: user,
+    });
   }
 }
