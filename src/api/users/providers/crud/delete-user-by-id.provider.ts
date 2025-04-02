6f304d1581c702606e0d99893b9cb7a68db73447
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-04-02 12:01:28 +0300

    main:finish crud on users table | find all users and delete user

--- src/api/users/providers/crud/delete-user-by-id.provider.ts
+++ src/api/users/providers/crud/delete-user-by-id.provider.ts
@@ -1,8 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { PrismaService } from 'src/common/prisma/prisma.service';
 
 @Injectable()
 export class DeleteUserByIdProvider {
-  deleteUserById(id: string) {
-    return `This action removes a #${id} user`;
+  constructor(private readonly prisma: PrismaService) {}
+  async deleteUserById(id: string) {
+    try {
+      const users = await this.prisma.user.delete({ where: { id } });
+      return users;
+    } catch (err) {
+      console.log('Error delete user: ', err);
+      throw new InternalServerErrorException(
+        'Unable to delete user. please try again later.',
+      );
+    }
   }
 }
--- src/api/users/providers/crud/find-all-users.provider.ts
+++ src/api/users/providers/crud/find-all-users.provider.ts
@@ -1,8 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { PrismaService } from 'src/common/prisma/prisma.service';
 
 @Injectable()
 export class FindAllUsersProvider {
-  findAllUsers() {
-    return `This action returns all users`;
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findAllUsers() {
+    try {
+      const users = await this.prisma.user.findMany();
+      return users;
+    } catch (err) {
+      console.log('Error find all users: ', err);
+      throw new InternalServerErrorException(
+        'Unable to find all user. please try again later.',
+      );
+    }
   }
 }
