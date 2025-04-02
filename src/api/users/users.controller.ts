d118c496bf9da828694610b4ff0c53d0a97cc8d2
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-04-02 09:44:58 +0300

    main:docs: add swagger doc to users controller

--- src/api/users/users.controller.ts
+++ src/api/users/users.controller.ts
@@ -11,26 +11,84 @@ import {
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOperation,
+  ApiParam,
+} from '@nestjs/swagger';
+import { AuthType } from '../auth/enums/auth-type.enum';
+import { Auth, Role } from '../auth/decorators';
+import { UserRole } from '@prisma/client';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @ApiOperation({
+    summary: 'Create a User',
+    description:
+      'Create a User, user this route to create a new user. Admins only',
+  })
+  @ApiBody({
+    required: true,
+    type: CreateUserDto,
+  })
+  @ApiBearerAuth()
+  @Auth(AuthType.BEARER)
+  @Role(UserRole.ADMIN)
   @Post()
   createUser(@Body() createUserDto: CreateUserDto) {
     return this.usersService.createUser(createUserDto);
   }
 
+  @ApiOperation({
+    summary: 'Find All Users',
+    description: 'Find All Users, use this route to get all users. Admins only',
+  })
   @Get()
+  @ApiBearerAuth()
+  @Auth(AuthType.BEARER)
+  @Role(UserRole.ADMIN)
   findAllUsers() {
     return this.usersService.findAllUsers();
   }
 
+  @ApiOperation({
+    summary: 'Find User By ID',
+    description:
+      'Find User By ID, use this route to get user by id. Admins only',
+  })
+  @ApiParam({
+    name: 'id',
+    description: 'id of the user',
+    required: true,
+  })
+  @ApiBearerAuth()
+  @Auth(AuthType.BEARER)
+  @Role(UserRole.ADMIN)
   @Get(':id')
   findUserById(@Param('id', ParseUUIDPipe) id: string) {
     return this.usersService.findUserById(id);
   }
 
+  @ApiOperation({
+    summary: 'Update User By ID',
+    description:
+      'Update User By ID, use this route to update user by id. Admins only',
+  })
+  @ApiParam({
+    name: 'id',
+    description: 'id of the user',
+    required: true,
+  })
+  @ApiBody({
+    required: true,
+    type: UpdateUserDto,
+  })
+  @ApiBearerAuth()
+  @Auth(AuthType.BEARER)
+  @Role(UserRole.ADMIN)
   @Patch(':id')
   updateUserById(
     @Param('id', ParseUUIDPipe) id: string,
@@ -39,6 +97,19 @@ export class UsersController {
     return this.usersService.updateUserById(id, updateUserDto);
   }
 
+  @ApiOperation({
+    summary: 'Delete User By ID',
+    description:
+      'Delete User By ID, use this route to delete user by id. Admins only',
+  })
+  @ApiParam({
+    name: 'id',
+    description: 'id of the user',
+    required: true,
+  })
+  @ApiBearerAuth()
+  @Auth(AuthType.BEARER)
+  @Role(UserRole.ADMIN)
   @Delete(':id')
   deleteUserById(@Param('id', ParseUUIDPipe) id: string) {
     return this.usersService.deleteUserById(id);
