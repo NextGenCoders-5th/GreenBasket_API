5d9830b35bea6417721113d6227e5fc2ac44e224
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-04-02 10:03:44 +0300

    main:feat: add create user dto with doc

--- src/api/users/dto/create-user.dto.ts
+++ src/api/users/dto/create-user.dto.ts
@@ -1,1 +1,48 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { UserRole } from '@prisma/client';
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsPhoneNumber,
+  IsString,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @ApiProperty({
+    description: 'user first name',
+  })
+  @IsNotEmpty()
+  @IsString()
+  firstName: string;
+
+  @ApiProperty({
+    description: 'user last name',
+  })
+  @IsNotEmpty()
+  @IsString()
+  lastName: string;
+
+  @ApiProperty({
+    description: 'user email',
+  })
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @ApiProperty({
+    description: 'user phone number',
+  })
+  @IsNotEmpty()
+  @IsPhoneNumber()
+  phoneNumber: string;
+
+  @ApiProperty({
+    description: 'user role',
+    enum: UserRole,
+    example: UserRole.CUSTOMER,
+  })
+  @IsNotEmpty()
+  @IsEnum(UserRole)
+  role: UserRole;
+}
