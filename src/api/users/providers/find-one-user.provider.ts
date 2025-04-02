84136b8e31df434e01531e7c022799275961161e
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-04-02 11:28:44 +0300

    main:feat: implement find one user by options provider

--- src/api/users/providers/find-one-user.provider.ts
+++ src/api/users/providers/find-one-user.provider.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { User } from '@prisma/client';
+import { PrismaService } from 'src/common/prisma/prisma.service';
 
 @Injectable()
 export class FindOneUserProvider {
-  findOneUser(options: Partial<User>) {
-    return 'this action not implemented';
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findOneUser(options: Partial<User>) {
+    try {
+      return await this.prisma.user.findFirst({ where: options });
+    } catch (err) {
+      console.log('find one user provider: ', err);
+      throw new InternalServerErrorException(
+        'Unable to fetch a user. please try again later.',
+      );
+    }
   }
 }
