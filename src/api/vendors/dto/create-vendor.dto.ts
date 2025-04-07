c6696d02878ef0557222ed755e6b08f46fbd143b
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-04-07 20:40:06 +0300

    main:feat: create all module providers on vendors

--- src/api/vendors/dto/create-vendor.dto.ts
+++ src/api/vendors/dto/create-vendor.dto.ts
@@ -1,1 +1,32 @@
-export class CreateVendorDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsPhoneNumber, IsString } from 'class-validator';
+
+export class CreateVendorDto {
+  @ApiProperty({
+    description: 'business name',
+  })
+  @IsNotEmpty()
+  @IsString()
+  business_name: string;
+
+  @ApiProperty({
+    description: 'business email',
+  })
+  @IsNotEmpty()
+  @IsEmail()
+  business_email: string;
+
+  @ApiProperty({
+    description: 'business phone number',
+  })
+  @IsNotEmpty()
+  @IsPhoneNumber()
+  @IsString()
+  phone_number: string;
+
+  @ApiProperty({
+    description: 'business logo',
+    required: true,
+  })
+  logo_url: string;
+}
--- src/api/vendors/vendors.module.ts
+++ src/api/vendors/vendors.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
-import { VendorsService } from './vendors.service';
+import { CreateVendorProvider } from './providers/create-vendor.provider';
+import { DeleteVendorProvider } from './providers/delete-vendor.provider';
+import { FindAllVendorsProvider } from './providers/find-all-vendors.provider';
+import { FindOneVendorProvider } from './providers/find-one-vendor.provider';
+import { FindVendorByIdProvider } from './providers/find-vendor-by-id.provider';
+import { UpdateVendorProvider } from './providers/update-vendor.provider';
 import { VendorsController } from './vendors.controller';
+import { VendorsService } from './vendors.service';
 
 @Module({
   controllers: [VendorsController],
-  providers: [VendorsService],
+  providers: [
+    VendorsService,
+    UpdateVendorProvider,
+    CreateVendorProvider,
+    DeleteVendorProvider,
+    FindAllVendorsProvider,
+    FindOneVendorProvider,
+    FindVendorByIdProvider,
+  ],
 })
 export class VendorsModule {}
