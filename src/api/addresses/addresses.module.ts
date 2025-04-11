6881d02342b0751d19224ca4403df151114f168b
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-04-11 19:00:37 +0300

    main:feat: create all modules needed for addresses resource

--- src/api/addresses/addresses.module.ts
+++ src/api/addresses/addresses.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AddressesService } from './addresses.service';
 import { AddressesController } from './addresses.controller';
+import { AddressesService } from './addresses.service';
+import { CreateUserAddressProvider } from './providers/create-user-address.provider';
+import { CreateVendorAddressProvider } from './providers/create-vendor-address.provider';
+import { DeleteAddressProvider } from './providers/delete-address.provider';
+import { FindAddressByIdProvider } from './providers/find-address-by-id.provider';
+import { FindAllAddressesProvider } from './providers/find-all-addresses.provider';
+import { FindOneAddressProvider } from './providers/find-one-address.provider';
+import { UpdateAddressByIdProvider } from './providers/update-address-by-id.provider';
 
 @Module({
   controllers: [AddressesController],
-  providers: [AddressesService],
+  providers: [
+    AddressesService,
+    FindAddressByIdProvider,
+    FindAllAddressesProvider,
+    FindOneAddressProvider,
+    CreateUserAddressProvider,
+    CreateVendorAddressProvider,
+    DeleteAddressProvider,
+    UpdateAddressByIdProvider,
+  ],
 })
 export class AddressesModule {}
