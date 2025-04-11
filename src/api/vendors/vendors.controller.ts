c7dea4c2b880bbd5b80c3704e2acfdc6e8ade78e
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-04-11 15:27:08 +0300

    main:docs: create vendor endpoint, logo file upload interceptor

--- src/api/vendors/vendors.controller.ts
+++ src/api/vendors/vendors.controller.ts
@@ -6,18 +6,58 @@ import {
   Patch,
   Param,
   Delete,
+  UseInterceptors,
+  UploadedFiles,
+  BadRequestException,
 } from '@nestjs/common';
 import { VendorsService } from './vendors.service';
 import { CreateVendorDto } from './dto/create-vendor.dto';
 import { UpdateVendorDto } from './dto/update-vendor.dto';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiConsumes,
+  ApiOperation,
+} from '@nestjs/swagger';
+import { FileUploadService } from 'src/common/file-upload/file-upload.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { FileUploadDirNames } from 'src/lib/constants/file-upload-dir-names';
 
 @Controller('vendors')
 export class VendorsController {
-  constructor(private readonly vendorsService: VendorsService) {}
+  constructor(
+    private readonly vendorsService: VendorsService,
+    private readonly fileUploadService: FileUploadService,
+  ) {}
 
+  @ApiOperation({
+    summary: 'create a vendor',
+    description: 'use this api endpoint to create a vendor.',
+  })
+  @ApiBody({
+    type: CreateVendorDto,
+    required: true,
+  })
+  @ApiConsumes('multipart/form-data')
+  @ApiBearerAuth()
+  @UseInterceptors(
+    FileInterceptor(
+      'logo',
+      FileUploadService.saveImageToStorage({
+        dirName: FileUploadDirNames.vendor,
+      }),
+    ),
+  )
   @Post()
-  create(@Body() createVendorDto: CreateVendorDto) {
-    return this.vendorsService.create(createVendorDto);
+  create(
+    @Body() createVendorDto: CreateVendorDto,
+    @UploadedFiles() logo: Express.Multer.File,
+  ) {
+    if (!logo) {
+      throw new BadRequestException('busines logo is required.');
+    }
+    createVendorDto.logo_url = this.fileUploadService.getFilePath(logo);
+    return this.vendorsService.createVendor(createVendorDto);
   }
 
   @Get()
--- src/api/vendors/vendors.service.ts
+++ src/api/vendors/vendors.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { CreateVendorDto } from './dto/create-vendor.dto';
 import { UpdateVendorDto } from './dto/update-vendor.dto';
+import { CreateVendorProvider } from './providers/create-vendor.provider';
 
 @Injectable()
 export class VendorsService {
-  create(createVendorDto: CreateVendorDto) {
-    return 'This action adds a new vendor';
+  constructor(private readonly createVendorProvider: CreateVendorProvider) {}
+  createVendor(createVendorDto: CreateVendorDto) {
+    return this.createVendorProvider.createVendor(createVendorDto);
   }
 
   findAll() {
