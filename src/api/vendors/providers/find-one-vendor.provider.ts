f2dc16bfebad98c4cfd084b1651e8de255b06dec
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-04-11 15:16:27 +0300

    main:feat: implement find one vendor by condition provider

--- src/api/vendors/providers/find-one-vendor.provider.ts
+++ src/api/vendors/providers/find-one-vendor.provider.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { Vendor } from '@prisma/client';
+import { PrismaService } from 'src/common/prisma/prisma.service';
 
 @Injectable()
-export class FindOneVendorProvider {}
+export class FindOneVendorProvider {
+  constructor(private readonly prisma: PrismaService) {}
+
+  public async findOneVendor(options: Partial<Vendor>) {
+    try {
+      const vendor = await this.prisma.vendor.findFirst({
+        where: options,
+      });
+      return vendor;
+    } catch (err) {
+      console.log('find one user by options: ', err);
+      throw new InternalServerErrorException(
+        'Unable to find user, Please try again later.',
+      );
+    }
+  }
+}
