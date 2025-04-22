c5b53606ec19443cbf8e10c45ffe65c2d98773ce
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-04-22 09:39:07 +0300

    main:docs: add swagger description on getInfor endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { AuthType } from './api/auth/enums/auth-type.enum';
 import { Auth } from './api/auth/decorators';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('/') // <- disables global prefix
 @Auth(AuthType.NONE)
 export class AppController {
+  @ApiOperation({
+    summary: 'Get API information',
+    description: 'Get API information',
+  })
   @Get()
   getInfo() {
     return {
