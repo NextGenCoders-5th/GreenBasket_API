803f90bcf8bbf087e2e56c77d8c9d6d370048929
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-05-06 12:33:59 +0300

    main:fix: the backend url

--- src/common/configuration/config/app.config.ts
+++ src/common/configuration/config/app.config.ts
@@ -4,8 +4,14 @@ export default registerAs('appConfig', () => ({
   appName: process.env.APP_NAME,
   apiVersion: process.env.API_VERSION,
   apiPrefix: process.env.API_PREFIX,
-  backendUrl: process.env.BACKEND_URL,
-  frontendUrl: process.env.FRONTEND_URL,
+  backendUrl:
+    process.env.NODE_ENV === 'development'
+      ? process.env.BACKEND_URL_DEV
+      : process.env.BACKEND_URL_PROD,
+  frontendUrl:
+    process.env.NODE_ENV === 'development'
+      ? process.env.FRONTEND_URL_DEV
+      : process.env.FRONTEND_URL_PROD,
   resetPasswordFrontendUrl: process.env.RESET_PASSWORD_FRONTEND_URL,
   defaultSysPassword: process.env.DEFAULT_SYS_PASSWORD,
 }));
