2e23e8f464b6750835f0483cff5f62952d678d23
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-05-24 06:43:47 +0300

    main:feat: update user dto

--- src/api/users/dto/update-user-data.dto.ts
+++ src/api/users/dto/update-user-data.dto.ts
@@ -1,6 +1,14 @@
-import { OmitType, PartialType } from '@nestjs/swagger';
+import { PartialType, OmitType, IntersectionType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
+import { CompleteOnboardingDto } from './complete-onboarding.dto';
 
-export class UpdateUserDataDto extends PartialType(
-  OmitType(CreateUserDto, ['role'] as const),
+export class UpdateUserDataDto extends IntersectionType(
+  PartialType(OmitType(CreateUserDto, ['role'] as const)),
+  PartialType(
+    OmitType(CompleteOnboardingDto, [
+      'userId',
+      'first_name',
+      'last_name',
+    ] as const),
+  ),
 ) {}
