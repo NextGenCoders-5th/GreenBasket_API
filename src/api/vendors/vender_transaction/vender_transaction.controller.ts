b6491f340a33198ef977623ef029e17fc3791cf4
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-05-26 20:53:01 +0300

    main:feat: implement transaction apis

--- src/api/vendors/vender_transaction/vender_transaction.controller.ts
+++ src/api/vendors/vender_transaction/vender_transaction.controller.ts
@@ -1,4 +1,74 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  InternalServerErrorException,
+  NotFoundException,
+  Query,
+} from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation, ApiQuery } from '@nestjs/swagger';
+import { UserRole, Vendor } from '@prisma/client';
+import { ActiveUser, Role } from 'src/api/auth/decorators';
+import { IActiveUserData } from 'src/api/auth/interfaces/active-user-data.interface';
+import { PrismaService } from 'src/common/prisma/prisma.service';
+import { VenderTransactionService } from './vender_transaction.service';
 
-@Controller('vender/transaction')
-export class VenderTransactionController {}
+@Controller('vendor/transaction')
+export class VenderTransactionController {
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly venderTransactionService: VenderTransactionService,
+  ) {}
+  // get all transactions
+  @ApiOperation({
+    summary: 'Get All Transactions',
+    description:
+      'Get all taransactions. admins can use this route to get all transaction history.',
+  })
+  @ApiBearerAuth()
+  @Role(UserRole.ADMIN)
+  @Get()
+  getAllTransactions() {
+    return this.venderTransactionService.getAllTransactions();
+  }
+
+  // get vendor transaction
+  @ApiOperation({
+    summary: 'Get Vendor Transaction',
+    description:
+      'Get Vendor Transaction. admins and vendors can use this route to get transaction history',
+  })
+  @ApiQuery({
+    name: 'id',
+    description:
+      'vendor id. admins can only get detail transaction by sending id. vendors can get their own only',
+    required: false,
+  })
+  @ApiBearerAuth()
+  @Role(UserRole.ADMIN, UserRole.VENDOR)
+  @Get('/by-vendor')
+  async getVendorTransactions(
+    @Query('id') vendorId: string,
+    @ActiveUser() activeUserData: IActiveUserData,
+  ) {
+    const { role, sub: userId } = activeUserData;
+    let vendor: Vendor;
+    try {
+      vendor = await this.prisma.vendor.findUnique({
+        where: {
+          ...(role === UserRole.VENDOR && { userId }),
+          ...(role === UserRole.ADMIN && { vendorId }),
+        },
+      });
+    } catch (err) {
+      console.log(err);
+      throw new InternalServerErrorException(
+        'Unable to find vendor, please try again later.',
+      );
+    }
+
+    if (!vendor) {
+      throw new NotFoundException('vendor not found.');
+    }
+    return this.venderTransactionService.getVendorTransactions(vendor.id);
+  }
+}
--- src/api/vendors/vender_transaction/vender_transaction.service.ts
+++ src/api/vendors/vender_transaction/vender_transaction.service.ts
@@ -1,4 +1,61 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { PrismaService } from 'src/common/prisma/prisma.service';
+import { CreateApiResponse } from 'src/lib/utils/create-api-response.util';
 
 @Injectable()
-export class VenderTransactionService {}
+export class VenderTransactionService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  // get all transactions
+  public async getAllTransactions() {
+    try {
+      const transactions = await this.prisma.vendorTransaction.findMany({
+        orderBy: {
+          createdAt: 'desc',
+        },
+      });
+
+      return CreateApiResponse({
+        status: 'success',
+        message: 'get all transactions successfull',
+        data: transactions,
+      });
+    } catch (err) {
+      console.log(err);
+      throw new InternalServerErrorException(
+        'Unable to fetch transactions. please try again later.',
+      );
+    }
+  }
+
+  // get vendor transaction
+  public async getVendorTransactions(vendorId: string) {
+    if (!vendorId) {
+      throw new BadRequestException('vendorId is required');
+    }
+
+    try {
+      const transactions = await this.prisma.vendorTransaction.findMany({
+        where: {
+          vendor_balance: {
+            vendorId,
+          },
+        },
+      });
+      return CreateApiResponse({
+        status: 'success',
+        message: 'get vendor transactions successfull.',
+        data: transactions,
+      });
+    } catch (err) {
+      console.log(err);
+      throw new InternalServerErrorException(
+        'Unable to fetch vendor transactions. please try again later.',
+      );
+    }
+  }
+}
